6048c0f6d9f29b7b2d4839383d2a3f8d56e04eda
Denis Queiroz <denisxx07@hotmail.com>
2021-05-24 20:19:34 -0300

    Implementacao do model Event e layout

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,26 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
     public function index() {
-        $nome = "Matheus";
-        $idade = 29;
+        $events = Event::all();
     
-        $arr = [10,20,30,40,50];
-    
-        $nomes = ["Matheus", "Maria", "João", "Saulo"];
-    
-        return view('welcome', 
-            [
-                'nome' => $nome, 
-                'idade2' => $idade, 
-                'profissao' => "Programador",
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]);
+        return view('welcome', [ 'events' => $events ]);
     }
 
     public function create() {
